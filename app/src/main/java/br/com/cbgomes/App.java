cd4af7385c54b4d2b9cc8f81bfe433f5aa13b97b
CarlosBarbosaFilho <cbarbosagomesfilho@gmail.com>
2022-01-17 11:10:25 -0300

    adjusts to module core and app: Remover anotation PackageScan

--- app/src/main/java/br/com/cbgomes/App.java
+++ app/src/main/java/br/com/cbgomes/App.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@ComponentScan (basePackages = {"br.com.cbgomes.*"})
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
